634372a3ef7a997fdf6bd7a1e8a3e6c64982a114
David Y. Stephenson <david@davidystephenson.com>
2022-03-15 10:16:03 +0100

    Handle change event

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
-import { FormEvent } from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react'
 import { Container, Heading, Input, FormControl, Button } from '@chakra-ui/react'
 
 function App (): JSX.Element {
+  const [message, setMessage] = useState('clear')
+
+  function handleChange (event: ChangeEvent): void {
+    setMessage('Changed')
+  }
+
   function handleSubmit (event: FormEvent): void {
     event.preventDefault()
     console.log('Submitted')
+    setMessage('Submitted')
   }
+
   return (
     <Container>
       <Heading>Icebreaker</Heading>
 
       <form onSubmit={handleSubmit}>
         <FormControl>
-          <Input />
+          <Input onChange={handleChange} />
         </FormControl>
 
         <Button type='submit'>Create Game</Button>
       </form>
+      {message}
     </Container>
   )
 }
