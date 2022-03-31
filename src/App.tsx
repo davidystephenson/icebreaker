e49f4f190de4ea9e1215a4896a39c23ea0092de5
David Y. Stephenson <david@davidystephenson.com>
2022-03-31 10:13:14 +0200

    Shows input

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,27 @@ import { Container, Heading, Input, FormControl, Button } from '@chakra-ui/react
 
 function App (): JSX.Element {
   const [message, setMessage] = useState('clear')
+  console.log('message', message)
+  const [gamesList, setGamesList] = useState(['first', 'second', 'third'])
+  function makeItem (game: string, index: number): JSX.Element {
+    const paragraph = <p key={index}>{game}</p>
+    return paragraph
+  }
+  const gameItems = gamesList.map(makeItem)
 
-  function handleChange (event: ChangeEvent): void {
-    setMessage('Changed')
+  function handleChange (event: ChangeEvent<HTMLInputElement>): void {
+    console.log('event', event)
+    console.log('event.target', event.target)
+    console.log('event.target.value', event.target.value)
+    setMessage(event.target.value)
   }
 
   function handleSubmit (event: FormEvent): void {
     event.preventDefault()
     console.log('Submitted')
     setMessage('Submitted')
+    const addedGamesList = [...gamesList, message]
+    setGamesList(addedGamesList)
   }
 
   return (
@@ -26,6 +38,7 @@ function App (): JSX.Element {
         <Button type='submit'>Create Game</Button>
       </form>
       {message}
+      {gameItems}
     </Container>
   )
 }
